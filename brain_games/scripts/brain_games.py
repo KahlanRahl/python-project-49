74038f87465341d75bad71a6e18efcbb9aae6a16
Kahlan_Amnell <kahlan_amnell@MacBook-Air-Kahlan-Amnell.local>
2023-01-10 18:40:25 +0300

    brain_games/scripts/brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-def main():
-    print ('Welcome to the Brain Games!')
 
 from brain_games.cli import welcome_user
+
+
+def main():
+    print('Welcome to the Brain Games!')
+
 
 welcome_user()
