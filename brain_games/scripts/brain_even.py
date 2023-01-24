a1dcc6ca8253a70f94c5651e63ad65a42542ce8f
Kahlan_Amnell <kahlan_amnell@MacBook-Air-Kahlan-Amnell.local>
2023-01-24 17:23:47 +0300

    brain_games/scripts/__pycache__/brain_even.cpython-310.pyc

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+
+import prompt
 import random
 from random import randint
 
@@ -12,24 +14,37 @@ def even_or_odd(number):
         return 'no'
 
 
+
+# глобальная функция приветствия и запроса имени
+def main():
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print('Hello, ' + name + "!")
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+
 # функция - цикл 
 # у пользователя есть три попытки ответить
-def sequence():
-    guess_left = 0
-    while guess_left <= 3:
+    def sequence():
+        guess_left = 0
+        while guess_left <= 3:
 # переменная - генерация случайного числа
-        random_number = random.randint(1, 20)
+            random_number = random.randint(1, 20)
 # программа задает вопрос пользователю со случайным числом
-        print("Question: " + str(random_number))
-        answer = input('Your answer:')
-        result = even_or_odd(random_number)
-        if result == answer:
-            print('Correct!')
-            guess_left += 1
-        else:
-            print(answer, "is wrong answer ;(. Correct answer was", result)
-            print('Let\'s try again', + name + '!')
-        break
-    if guess_left >= 3:
-        print('Congratulations!')
-sequence()
+            print("Question: " + str(random_number))
+            answer = input('Your answer:')
+            result = even_or_odd(random_number)
+            if result == answer:
+                print('Correct!')
+                guess_left += 1
+            else:
+                print(answer, "is wrong answer ;(. Correct answer was", result)
+                print('Let\'s try again, ' + name + '!')
+            break
+        if guess_left >= 3:
+            print('Congratulations!')
+    sequence()
+
+
+if __name__ == '__main__':
+    main() 
