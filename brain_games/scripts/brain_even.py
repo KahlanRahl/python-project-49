3b039e95d8ab361a735660be4d6b100a591e55d1
Kahlan_Amnell <kahlan_amnell@MacBook-Air-Kahlan-Amnell.local>
2023-01-29 20:37:06 +0300

    brain_games/scripts/__pycache__/brain_even.cpython-310.pyc

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -21,6 +21,7 @@ def main():
     name = prompt.string("May I have your name? ")
     print('Hello, ' + name + "!")
     print('Answer "yes" if the number is even, otherwise answer "no".')
+main()
 
 
 # описание логики игры
