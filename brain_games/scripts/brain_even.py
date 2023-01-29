01e193e9be8c36a1a16c81be0a6e33f204926364
Kahlan_Amnell <kahlan_amnell@MacBook-Air-Kahlan-Amnell.local>
2023-01-29 20:35:09 +0300

    brain_games/scripts/brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -6,7 +6,7 @@ import random
 from random import randint
 
 
-# функция для определения четности числа
+# функция определения четности нечетности числа
 def even_or_odd(number):
     if number % 2 == 0:
         return 'yes'
@@ -14,37 +14,33 @@ def even_or_odd(number):
         return 'no'
 
 
-
-# глобальная функция приветствия и запроса имени
+# функция приветствия пользователя
 def main():
+    global name
     print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
     print('Hello, ' + name + "!")
     print('Answer "yes" if the number is even, otherwise answer "no".')
 
 
-# функция - цикл 
-# у пользователя есть три попытки ответить
-    def sequence():
-        guess_left = 0
-        while guess_left <= 3:
-# переменная - генерация случайного числа
-            random_number = random.randint(1, 20)
-# программа задает вопрос пользователю со случайным числом
-            print("Question: " + str(random_number))
-            answer = input('Your answer:')
-            result = even_or_odd(random_number)
-            if result == answer:
-                print('Correct!')
-                guess_left += 1
-            else:
-                print(answer, "is wrong answer ;(. Correct answer was", result)
-                print('Let\'s try again, ' + name + '!')
-            break
-        if guess_left >= 3:
-            print('Congratulations!')
-    sequence()
+# описание логики игры
+guess_left = 0
+while guess_left < 3:
+    random_number = random.randint(1, 20)
+    print("Question: " + str(random_number))
+    answer = input('Your answer:')
+    result = even_or_odd(random_number)
+    if result == answer:
+        print('Correct!')
+        guess_left += 1
+    else:
+        print(answer, "is wrong answer ;(. Correct answer was", result)
+        print('Let\'s try again, ' + name + '!')
+        break
+
+if guess_left == 3:
+    print('Congratulations, ' + name + '!')
 
 
-if __name__ == '__main__':
-    main() 
+if name == 'main':
+    main()
