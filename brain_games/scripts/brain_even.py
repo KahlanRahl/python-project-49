14ec658558cc3562a529d9dd9603104e6be45f0f
Anna Gorbacheva <annagorbacheva@iMac-Anna.local>
2023-01-30 14:35:24 +0300

    brain_games/scripts/brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -6,7 +6,7 @@ import random
 from random import randint
 
 
-# функция определения четности нечетности числа
+# функция определения четности числа
 def even_or_odd(number):
     if number % 2 == 0:
         return 'yes'
@@ -14,32 +14,31 @@ def even_or_odd(number):
         return 'no'
 
 
-# функция приветствия пользователя
+# функция приветствия пользователя + задача игры
 def main():
-    global name
+    global name # для доступности переменной в других частях кодах, используем глобальность
     print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
     print('Hello, ' + name + "!")
     print('Answer "yes" if the number is even, otherwise answer "no".')
-main()
 
+main()  # вызываем основную функцию
 
-# описание логики игры
-guess_left = 0
-while guess_left < 3:
-    random_number = random.randint(1, 20)
+guess_left = 3  # вводим переменную с количеством оставшихся попыток
+while guess_left > 0:  # приступаем к циклу while
+    random_number = random.randint(1, 20) #генерируем случайное число
     print("Question: " + str(random_number))
     answer = input('Your answer:')
     result = even_or_odd(random_number)
-    if result == answer:
+    if result == answer:   # сверяем ответ пользователя и результат программы
         print('Correct!')
-        guess_left += 1
+        guess_left -= 1  # подсчитываем сколько осталось попыток
     else:
         print(answer, "is wrong answer ;(. Correct answer was", result)
         print('Let\'s try again, ' + name + '!')
         break
 
-if guess_left == 3:
+if guess_left == 0:
     print('Congratulations, ' + name + '!')
 
 
