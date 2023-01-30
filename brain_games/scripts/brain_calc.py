318c4a2bc9c38b7ac608ff17e00e693646c9abe9
Anna Gorbacheva <annagorbacheva@iMac-Anna.local>
2023-01-30 18:29:36 +0300

    brain_games/scripts/__pycache__/brain_calc.cpython-310.pyc

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -7,46 +7,36 @@ from random import randint
 from operator import add, sub, mul
 
 
-print("Welcome to the Brain Games!")
-name = prompt.string("May I have your name? ")
-
-# глобальная функция приветствия и запроса имени
+# функция приветствия пользователя
 def main():
-    #print("Welcome to the Brain Games!")
-    #name = prompt.string("May I have your name? ")
-    print('Hello, ' + name + "!")
+  print("Welcome to the Brain Games!")
+  name = prompt.string("May I have your name? ")
+  print('Hello, ' + name + "!")
 
-main()
+  operators = {"+": add, "-": sub, "*": mul}  # создаем словарь
+  tries = 3  # переменная на количество попыток
+  print('What is the result of the expression?')
+
+  keys = list(operators)  # ключи к словарю
+
+  for i in range(3):
+    random_num1 = random.randint(1, 30)
+    random_num2 = random.randint(1, 30)
+    operator = random.choice(keys)
+    counting = str(random_num1) + str(operator) + str(random_num2)
+    print("Question: " + counting)
+    result = operators[operator](random_num1, random_num2)
+    answer = int(input('Your answer:'))
+    if answer == result:
+      print('Correct!')
+      tries += 1
+    else:
+      print(answer, "is wrong answer ;(. Correct answer was", result)
+      print('Let\'s try again, ' + name + '!')
+      break
+  if tries >= 3:
+    print('Congratulations!')
 
 
-operators = {"+": add, "-": sub, "*": mul}
-tries = 3 # количество попыток пользователя
-print('What is the result of the expression?')
-
-
-# создаем список/list ключей к словарю для передачи к random.choice
-keys = list(operators)
-
-
-# используем функцию range()
-for i in range(3):
-  random_num1 = random.randint(1, 30)
-  random_num2 = random.randint(1, 30)
-  operator = random.choice(keys)
-#print("Question: " + str(random_num1) + str(operator) + str(random_num2))
-  counting = str(random_num1) + str(operator) + str(random_num2)
-  print("Question: " + counting)
-  result = operators[operator](random_num1, random_num2)
-  answer = int(input('Your answer:'))
-  if answer == result:
-    print('Correct!')
-    tries += 1
-  else:
-    print(answer, "is wrong answer ;(. Correct answer was", result)
-    print('Let\'s try again, ' + name + '!')
-  break
-if tries >= 3:
-  print('Congratulations!')
-
-#if __name__ == '__main__':
-  #main() 
+if __name__ == '__main__':
+  main()
