8449e31c086dc1c8102431e68d0b76d6b4190574
Anna Gorbacheva <annagorbacheva@iMac-Anna.local>
2023-02-01 15:29:18 +0300

    brain_games/scripts/brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -4,68 +4,50 @@ import prompt
 import random
 
 
-def main():
-    # Print the welcome message
-    print("Welcome to the Brain Games!")
+def calculate_expression(num1, num2, operation):
+    """
+    Function to perform the calculation for a given expression
+    """
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
 
-    # Get the name of the user
+
+def main():
+    print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}!")
     print("What is the result of the expression?")
 
-    # Loop to repeat the game 3 times
     for i in range(3):
-        # Get two random numbers
+        # Generate two random numbers and a random operation
         num1 = random.randint(0, 100)
         num2 = random.randint(0, 100)
-
-        # List of possible operations
-        operations = ["+", "-", "*"]
-
-        # Choosing a random operation from the list
-        operation = random.choice(operations)
-
-        # Forming the question string
+        operation = random.choice(["+", "-", "*"])
         question = f"{num1} {operation} {num2}"
+        correct_answer = calculate_expression(num1, num2, operation)
 
-        # Variable to store the correct answer
-        correct_answer = None
-
-        # Checking which operation was chosen and performing the calculation
-        if operation == "+":
-            correct_answer = num1 + num2
-        elif operation == "-":
-            correct_answer = num1 - num2
-        elif operation == "*":
-            correct_answer = num1 * num2
-
-        # Print the question
         print(f"Question: {question}")
-
-        # Get the answer from the user
         answer = input("Your answer: ").strip()
-
-        # Converting the user's answer to an integer
         try:
             answer = int(answer)
         except ValueError:
-            # If the answer is not a number, it is considered an error
             print(f"{answer} is wrong answer ;(."
                   f"Correct answer was {correct_answer}")
             print(f"Let's try again, {name}!")
             return
 
-        # Checking if the user's answer is correct
         if answer == correct_answer:
             print("Correct!")
         else:
-            # If the answer is incorrect, the game ends
             print(f"{answer} is wrong answer ;(."
                   f"Correct answer was {correct_answer}")
             print(f"Let's try again, {name}!")
             return
 
-    # If all answers are correct, the user wins the game
     print(f"Congratulations, {name}!")
 
 
