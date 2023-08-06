367c932e239382ad72b38182d8c6a0c3447e5117
Kahlan_Amnell <kahlan_amnell@MacBook-Air-Kahlan-Amnell.local>
2023-08-06 14:58:49 +0300

    .gitignore

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,6 +3,4 @@ import prompt
 
 def welcome_user():
     name = prompt.string('May I have your name? ')
-    print('Hello, ' + name)
-    if __name__ == '__main__':
-        welcome_user()
+    print(f'Hello, {name}!')
