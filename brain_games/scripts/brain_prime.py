b0ea01770dbc17c865ebb18cba5f4756f70c8fcd
Kahlan_Amnell <kahlan_amnell@MacBook-Air-Kahlan-Amnell.local>
2023-08-06 15:00:23 +0300

    brain_games/scripts/brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,40 +1,15 @@
 #!/usr/bin/env python3
 
-import prompt
-import random
+"""Prime"""
+
+from brain_games.games_logic import play
+from brain_games.games import prime
 
 
 def main():
-    print("Welcome to the Brain Games!")
-    name = prompt.string("May I have your name? ")
-    print(f"Hello, {name}!")
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    for i in range(3):
-        number = random.randint(1, 100)
-        if is_prime(number):
-            answer = "yes"
-        else:
-            answer = "no"
-        print(f"Question: {number}")
-        user_answer = input("Your answer: ")
-        if user_answer == answer:
-            print("Correct!")
-        else:
-            print(f"'{user_answer}' is wrong answer ;(."
-                  f"Correct answer was '{answer}'.")
-            print(f"Let's try again, {name}!")
-            return
-    print(f"Congratulations, {name}!")
+    """Start to play Prime Game"""
+    play(prime)
 
 
-def is_prime(number):
-    if number <= 1:
-        return False
-    for i in range(2, number):
-        if number % i == 0:
-            return False
-    return True
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,48 +1,15 @@
 #!/usr/bin/env python3
 
+"""Progression Game"""
 
-import prompt
-import random
+from brain_games.games_logic import play
+from brain_games.games import progression
 
 
-# Основная функция
 def main():
-    print("Welcome to the Brain Games!")
-    name = prompt.string("May I have your name? ")
-    print(f"Hello, {name}!")
-    print("What number is missing in the progression?")
-    for j in range(3):  # описываем правила с тремя попытками
-        progression = generate_progression()  # получаем прогрессию чисел
-# случайная позиция для недостающего числа
-        missing_number_index = random.randint(0, len(progression) - 1)
-# копируем список progression
-        prog = progression[:]
-# заменяем отсутствующее число в списке-копии выше точками
-        prog[missing_number_index] = ".."
-# соединяем элементы списка-копии `prog`
-# в виде строки с пробелом между каждым элементом
-        question = " ".join(map(str, prog))
-        vacancy = str(progression[missing_number_index])   # правильный ответ
-        print(f"Question: {question}")
-        user_answer = input()
-# проверяем совпадает ли ответ пользователя с правильным ответом
-        if user_answer == vacancy:
-            print("Correct!")
-        else:
-            print(f"'{user_answer}' is wrong answer ;(."
-                  f"Correct answer was '{vacancy}'.")
-            print(f"Let's try again, {name}!")
-            return
-    print(f"Congratulations, {name}!")
+    """Start to play Progression Game"""
+    play(progression)
 
 
-# функция для генерации прогрессии чисел
-def generate_progression():
-    start = random.randint(1, 100)
-    step = random.randint(1, 10)
-    progression = [start + i * step for i in range(10)]
-    return progression
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
